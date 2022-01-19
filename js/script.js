501fb6c04a71203ad8bc0ed5d74f65a92777ef00
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-01-19 12:04:01 -0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function updatemenu() {
-  console.log('teste')
   if (document.getElementById('responsive-menu').checked == true) {
     document.getElementById('menu').style.borderBottomRightRadius = '0';
     document.getElementById('menu').style.borderBottomLeftRadius = '0';
